9ce348716523726ca82cb68d301989daaba56737
ludik <bakhanetssimon@gmail.com>
2026-01-03 03:22:10 +0700

    Initial Commit

---
+++ KanEduSphere/KanEduSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanEduSphereApp.swift
+//  KanEduSphere
+//
+//  Created by Simon Bakhanets on 03.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct KanEduSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanEduSphereTests/KanEduSphereTests.swift
@@ -0,0 +1,16 @@
+//
+//  KanEduSphereTests.swift
+//  KanEduSphereTests
+//
+//  Created by Simon Bakhanets on 03.01.2026.
+//
+
+import Testing
+
+struct KanEduSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
